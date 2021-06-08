fee5ffd9dd954fd035814d0283f1b041e600eb26
Stivn Nkolov <stivnnkolov@gmail.com>
2021-06-08 15:19:40 +0300

    updates on 06-Knight-Game.py

--- Python-Advanced/MULTIDIMENSIONAL-LISTS-ex/06-Knight-Game.py
+++ Python-Advanced/MULTIDIMENSIONAL-LISTS-ex/06-Knight-Game.py
@@ -6,15 +6,24 @@ for row in range(n):
     matrix.append([el for el in input()])
 
 
-def count_kills(matr, row_index, col_index):
+def check_for_valid_index(*args):
+    possible_r = args[0]
+    possible_c = args[1]
+    dimensions = args[2]
+    if 0 <= possible_r < dimensions and 0 <= possible_c < dimensions:
+        return True
+    return False
+
+
+def count_kills(data, row_index, col_index):
     kills = 0
     movement_rows = [-2, -2, 2, 2, 1, 1, -1, -1]
     movement_cols = [-1, 1, -1, 1, -2, 2, -2, 2]
     for index in range(len(movement_rows)):
         possible_row = row_index + movement_rows[index]
-        possibler_col = col_index + movement_cols[index]
-        if 0 <= possible_row < len(matrix) and 0 <= possibler_col < len(matrix):
-            possible_position = matrix[possible_row][possibler_col]
+        possible_col = col_index + movement_cols[index]
+        if check_for_valid_index(possible_row, possible_col, n):
+            possible_position = data[possible_row][possible_col]
             if possible_position == "K":
                 kills += 1
 
@@ -34,7 +43,6 @@ while True:
                     killer_position = [r, c]
     if killer_position:
         matrix[killer_position[0]][killer_position[1]] = "0"
-        # killer_position = []
         knights_removed += 1
     else:
         break
