56c432af5b5a5988fef65d1fb52c5dc3ccc8a1fd
Stivn Nkolov <stivnnkolov@gmail.com>
2021-06-09 12:20:32 +0300

    updating source code for 03-Capitals.py

--- Python-Advanced/Comprehension-ex/03-Capitals.py
+++ Python-Advanced/Comprehension-ex/03-Capitals.py
@@ -1,5 +1,9 @@
 country_names = input().split(", ")
-capital_name = input().split(", ")
+capital_names = input().split(", ")
 
-for index in range(len(country_names)):
-    print(f"{country_names[index]} -> {capital_name[index]}")
+# for index in range(len(country_names)):
+#     print(f"{country_names[index]} -> {capital_names[index]}")
+
+my_dict = {country_names[index]: capital_names[index] for index in range(len(country_names))}
+
+[print(f"{country} -> {capital}") for country, capital in my_dict.items()]
