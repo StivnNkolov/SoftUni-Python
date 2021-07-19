8506589b22da8878ff87370a1d3431c8d8aeee87
Stivan Nikolov <77329998+StivnNkolov@users.noreply.github.com>
2021-07-19 20:41:34 +0300

    Delete Python-OOP/08-Encapsulation-ex/WildCatZoo01 directory

--- Python-OOP/08-Encapsulation-ex/WildCatZoo01/tiger.py
+++
@@ -1,8 +0,0 @@
-from WildCatZoo01.animal import Animal
-
-
-class Tiger(Animal):
-    TIGER_MONEY_FOR_CARE = 45
-
-    def __init__(self, name, gender, age):
-        super().__init__(name, gender, age, Tiger.TIGER_MONEY_FOR_CARE)
